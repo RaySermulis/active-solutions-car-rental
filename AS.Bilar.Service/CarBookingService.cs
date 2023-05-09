031bc7fe06ed5680613713014c216c262d28359d
RaySermulis <reinisps@gmail.com>
2023-05-09 15:24:42 +0200

    Add files via upload

---
+++ AS.Bilar.Service/CarBookingService.cs
@@ -0,0 +1,97 @@
+﻿using AS.Bilar.DAL.Enums;
+using AS.Bilar.DAL.Models;
+using AS.Bilar.DAL.Repositories;
+
+
+namespace AS.Bilar.Service
+{
+    public interface ICarBookingService
+    {
+        Task RegisterCarBooking(string bookingNumber, Car carInfo, Customer customerDetails);
+        Task FinishCarBooking(string bookingNumber);
+    }
+
+    public class CarBookingService : ICarBookingService
+    {
+        private readonly ICarBookingRepository _carBookingRepository;
+
+        private const double BaseDailyPrice = 500;
+        private const int BaseKmPrice = 20;
+
+        private const double KombiStandartTariff = 1.3;
+        private const double KombiTariff = 1;
+
+        private const double TruckTariff = 1.5;
+        private const double TruckStandartTariff = 1.5;
+
+
+        public CarBookingService(ICarBookingRepository carBookingRepository)
+        {
+            _carBookingRepository = carBookingRepository;
+        }
+
+        public async Task RegisterCarBooking(string bookingNumber, Car carInfo, Customer customerDetails)
+        {
+            var carBookingRecord = new CarBooking
+            {
+                ID = new Guid(),
+                BookingNumber = bookingNumber,
+                CarInfo = carInfo,
+                CustomerInfo = customerDetails,
+                CreatedDate = DateTime.Now,
+                BookingStatus = BookingStatus.BookingStarted
+            };
+
+            await _carBookingRepository.InsertCarBooking(carBookingRecord);
+        }
+
+        public async Task FinishCarBooking(string bookingNumber)
+        {
+            //Register a finished car rental booking
+            var existingBooking = await _carBookingRepository.GetCarBooking(bookingNumber);
+            if (existingBooking is not null)
+            {
+                var finishedCarBooking = new CarBooking
+                {
+                    ID = new Guid(),
+                    BookingNumber = bookingNumber,
+                    CarInfo = existingBooking.CarInfo,
+                    CustomerInfo = existingBooking.CustomerInfo,
+                    CreatedDate = DateTime.Now,
+                    BookingStatus = BookingStatus.BookingCompleted
+                };
+
+                await _carBookingRepository.InsertCarBooking(finishedCarBooking);
+            }
+            else
+            {
+                //ToDo something to notify/throw etc...
+                return;
+            }
+
+            //Calculate the price for the finished booking
+            var completeBookingSet = await _carBookingRepository.GetCarBookings(bookingNumber);
+            switch (existingBooking.CarInfo.CarType)
+            {
+                case CarType.Small:
+                    {
+                        PriceCalculator.CalculateCarPrice(BaseDailyPrice, completeBookingSet);
+                        break;
+                    }
+                case CarType.Kombi:
+                    {
+                        PriceCalculator.CalculateCarPrice(BaseDailyPrice, BaseKmPrice, completeBookingSet, KombiStandartTariff, KombiTariff);
+                        break;
+                    }
+                case CarType.Truck:
+                    {
+                        PriceCalculator.CalculateCarPrice(BaseDailyPrice, BaseKmPrice, completeBookingSet, TruckStandartTariff, TruckTariff);
+                        break;
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
+
---
+++ AS.Bilar.Service/PriceCalculator.cs
@@ -0,0 +1,42 @@
+﻿using AS.Bilar.DAL.Enums;
+using AS.Bilar.DAL.Models;
+
+namespace AS.Bilar.Service
+{
+    public static class PriceCalculator
+    {
+        public static double CalculateCarPrice(double baseDailyPrice, List<CarBooking> completeBookings)
+        {
+            if (!completeBookings.Select(x => x.BookingStatus).Contains(BookingStatus.BookingCompleted))
+            {
+                //ToDo something to notify/throw etc...
+            }
+
+            var bookingDates = completeBookings.Select(x => x.CreatedDate).OrderBy(x => x.TimeOfDay).ToList();
+            var amountOfDays = (bookingDates.Max() - bookingDates.Min()).TotalDays;
+
+            //if same day return
+            if (amountOfDays == 0)
+            {
+                amountOfDays = 1;
+            }
+            return baseDailyPrice * amountOfDays;
+        }
+
+        public static double CalculateCarPrice(double baseDailyPrice, double baseKmPrice, List<CarBooking> completeBookings, double standartTariff, double tariff)
+        {
+            if (!completeBookings.Select(x => x.BookingStatus).Contains(BookingStatus.BookingCompleted))
+            {
+                //ToDo something to notify/throw etc...
+            }
+
+            var bookingDates = completeBookings.Select(x => x.CreatedDate).OrderBy(x => x.TimeOfDay).ToList();
+            var amountOfDays = (bookingDates.Max() - bookingDates.Min()).TotalDays;
+
+            var mileageRecords = completeBookings.Select(x => x.CarInfo.Mileage).ToList();
+            var mileageDriven = mileageRecords.Max() - mileageRecords.Min();
+
+            return baseDailyPrice * amountOfDays * standartTariff + baseKmPrice * mileageDriven * tariff;
+        }
+    }
+}
